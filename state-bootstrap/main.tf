21e5d5adcb0050b54d78787eebd597587fcde85a
Willie <williesbeat18@yahoo.com>
2026-06-30 13:50:08 +0100

    third run

--- state-bootstrap/main.tf
+++ state-bootstrap/main.tf
@@ -21,7 +21,22 @@ resource "aws_s3_bucket_versioning" "tfstate" {
   }
 }
 
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  for_each       = toset(local.envs)
+  name           = "ec2-terraform-state-lock-${each.key}"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "Terraform State Lock Table"
+    Environment = each.key
+  }
+}
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
   for_each = aws_s3_bucket.tfstate
